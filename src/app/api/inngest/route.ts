b796210b982bc123d9a89ff99f08fc1df80e4e73
pvsmda <spaulovictor813@gmail.com>
2025-06-29 21:24:58 -0300

    Messages

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,10 +1,10 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/inngest/client";
-import { helloWorld } from "@/inngest/functions";
+import { codeAgentFunction } from "@/inngest/functions";
 
 // create an api that servers zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [helloWorld],
+  functions: [codeAgentFunction],
 });
