04959296819e5411f74eefed0a8017b497bb4fc9
pvsmda <spaulovictor813@gmail.com>
2025-06-30 11:20:17 -0300

    Projects

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -171,6 +171,7 @@ export const codeAgentFunction = inngest.createFunction(
       if (isError) {
         return await prisma.message.create({
           data: {
+            projectId: event.data.projectId,
             content: "Something went wrong. Please try again.",
             role: "ASSISTANT",
             type: "ERROR",
@@ -182,6 +183,7 @@ export const codeAgentFunction = inngest.createFunction(
     await step.run("save-result", async () => {
       return await prisma.message.create({
         data: {
+          projectId: event.data.projectId,
           content: result.state.data.summary,
           role: "ASSISTANT",
           type: "RESULT",
