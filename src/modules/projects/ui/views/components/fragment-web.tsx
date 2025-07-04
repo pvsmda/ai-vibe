b2342485091e8f09f79f49161a2fe38bdcfda36e
pvsmda <spaulovictor813@gmail.com>
2025-07-04 20:09:08 -0300

    Fragment View

---
+++ src/modules/projects/ui/views/components/fragment-web.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { ExternalLinkIcon, RefreshCcwIcon } from "lucide-react";
+
+import { Fragment } from "@/generated/prisma";
+import { Button } from "@/components/ui/button";
+import { Hint } from "@/components/hint";
+import { toast } from "sonner";
+
+interface Props {
+  data: Fragment;
+}
+
+export function FragmentWeb({ data }: Props) {
+  const [copied, setCopied] = useState(false);
+  const [fragmentKey, setFragmentKey] = useState(0);
+
+  const onRefresh = () => {
+    setFragmentKey((prev) => prev + 1);
+  };
+
+  const handleCopy = () => {
+    if (!navigator.clipboard) {
+      console.warn("Clipboard API not supported");
+      return;
+    }
+
+    navigator.clipboard
+      .writeText(data.sandboxUrl)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000);
+      })
+      .catch((err) => {
+        console.error("Failed to copy URL:", err);
+        toast.error("Failed to copy URL");
+      });
+  };
+
+  return (
+    <div className="flex flex-col w-full h-full">
+      <div className="p-2 border-b bg-sidebar flex items-center gap-x-2">
+        <Hint text="Refresh" side="bottom" align="start">
+          <Button size="sm" variant="outline" onClick={onRefresh}>
+            <RefreshCcwIcon />
+          </Button>
+        </Hint>
+
+        <Hint text="Copy URL" side="bottom">
+          <Button
+            size="sm"
+            variant="outline"
+            onClick={handleCopy}
+            disabled={!data.sandboxUrl || copied}
+            className="flex-1 justify-start text-start font-normal!"
+          >
+            <span className="truncate">{data.sandboxUrl}</span>
+          </Button>
+        </Hint>
+
+        <Hint text="Open in new tab" side="bottom" align="start">
+          <Button
+            size="sm"
+            disabled={!data.sandboxUrl}
+            variant="outline"
+            onClick={() => {
+              if (!data.sandboxUrl) return;
+              window.open(data.sandboxUrl, "_blank");
+            }}
+          >
+            <ExternalLinkIcon />
+          </Button>
+        </Hint>
+      </div>
+      <iframe
+        key={fragmentKey}
+        src={data.sandboxUrl}
+        sandbox="allow-forms allow-scripts allow-same-origin"
+        loading="lazy"
+        className="w-full h-full"
+      />
+    </div>
+  );
+}
